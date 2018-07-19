29fea2a577d847a1d76447778e0de9d310338f69
王瑶(滴滴研究院) <813393834@qq.com>
2018-07-19 20:58:09 +0800

    提交进度，还有bug

--- static/js/admin.js
+++ static/js/admin.js
@@ -76,6 +76,46 @@ var app = new Vue({
       self.authToken = getCookie('auth-token')
     })
     this.loadAllPosts()
+
+    // -----图片剪裁功能
+    this.$nextTick(function () {
+      var cropper = new Cropper({
+        aspectRatio: 'auto',
+        element: document.getElementById('cropper-target'),
+        previews: [
+          document.getElementById('preview-large'),
+          document.getElementById('preview-medium'),
+          document.getElementById('preview-small')
+        ],
+        onCroppedRectChange: function(rect) {
+          console.log(rect);
+        }
+      });
+      var input = document.getElementById('cropper-input');
+      console.log(input);
+      console.log(input);
+
+      input.onchange = function() {
+        if (typeof FileReader !== 'undefined') {
+          var reader = new FileReader();
+          reader.onload = function (event) {
+            cropper.setImage(event.target.result);
+          };
+          if (input.files && input.files[0]) {
+            reader.readAsDataURL(input.files[0]);
+          }
+        } else { // IE10-
+          input.select();
+          input.blur();
+
+          var src = document.selection.createRange().text;
+          cropper.setImage(src);
+        }
+      };
+    });
+
+    // -----end - 图片剪裁功能
+
   },
   methods: {
     /**
